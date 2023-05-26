4b689e1d72e9355c705f1bdce88fae576f5da2b5
Ingo Ruhnke <grumbel@gmail.com>
2023-05-26 21:57:00 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
   description = "A 2D platform game featuring Tux the penguin";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
